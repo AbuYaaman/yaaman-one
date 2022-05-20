13e043331da922f34793fbf602735f9091ae5c69
Mahmood Abubakar <mahmood@Mahmoods-MacBook-Pro.local>
2022-05-20 16:14:06 +0100

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+
+const About = () => {
+    return (
+        <>
+        <Head>
+          <title>Ninja List | About</title>
+          <meta name="keywords" content="ninjas" />
+        </Head>
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quaerat ipsam hic error, atque ipsa, molestias temporibus reiciendis, corrupti dignissimos culpa fugit. Cumque numquam architecto quaerat quasi placeat laudantium assumenda eaque.</p>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Explicabo voluptatibus sit vero necessitatibus illum alias optio nesciunt, mollitia architecto dicta modi facere expedita maxime quos? Commodi tenetur animi eaque dicta.</p>
+        </div>
+    </>
+    );
+}
+ 
+export default About;
